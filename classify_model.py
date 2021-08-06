ae40278abd02a51ef5b645a43a1ad527ca7501d1
LZT <csulzt@foxmail.com>
2021-08-06 12:11:53 +0800

    分离memory_network为单独的模块

--- classify_model.py
+++ classify_model.py
@@ -18,6 +18,10 @@ from memory_networks import MemoryNetwork
 warnings.filterwarnings('ignore')
 plm_path = 'chinese-roberta-wwm-ext'  # 该文件夹下存放三个文件（'vocab.txt', 'pytorch_model.bin', 'config.json'）
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+summarywrite_path = 'runs/ClassifyModel2.0'
+model_save_path = 'model/cls/'
+epochs = 2
+batch_size = 1
 
 train_dataset = SentenceDataset(data_path='data/classify_data/train_data.json')
 valid_dataset = SentenceDataset(data_path='data/classify_data/valid_data.json')
@@ -25,6 +29,7 @@ test_dataset = SentenceDataset(data_path='data/classify_data/test_data.json')
 train_length = train_dataset.get_length_of_single_judgment()
 valid_length = valid_dataset.get_length_of_single_judgment()
 test_length = test_dataset.get_length_of_single_judgment()
+mode_length = {'train': train_length, 'valid': valid_length, 'test': test_length}
 
 
 ### 1. 这种方式是不需要手动下载模型文件，在网速快的时候使用
@@ -35,36 +40,23 @@ test_length = test_dataset.get_length_of_single_judgment()
 # 加载bert模型，这个路径文件夹下有bert_config.json配置文件和model.bin模型权重文件
 # bert = BertModel.from_pretrained('chinese-roberta-wwm-ext')
 
-
 class SentenceClassifyModel(nn.Module):
-    def __init__(self, bert_path, mode='train', classes=2):
+    def __init__(self, bert_path, classes=2):
         super(SentenceClassifyModel, self).__init__()
         self.config = BertConfig.from_pretrained(bert_path)  # 导入模型超参数
         self.bert = BertModel.from_pretrained(bert_path)  # 加载预训练模型权重
         self.dropout = nn.Dropout(self.config.hidden_dropout_prob)
         self.classifier = nn.Linear(self.config.hidden_size, classes)  # 直接分类
-        self.mode = mode
-        self.length = train_length
-        self.memory_slot = torch.empty()
+        # 记忆网络模块
+        self.memory_network = MemoryNetwork(self.config.hidden_size)
+        self.classifier = nn.Linear(self.config.hidden_size * 2, classes)  # 直接分类
 
-    def forward(self, input_ids, attention_mask, token_type_ids=None):
+    def forward(self, input_ids, attention_mask, token_type_ids=None, mode='train'):
         outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
         out_pool = outputs[1]  # 池化后的输出 [bs, config.hidden_size]
+        out_pool = self.memory_network.get_memory_embedding(out_pool, mode_length, mode)  # 记忆网络模块
         logit = self.classifier(out_pool)  # [bs, classes]
         return logit
-
-    def get_memory_embedding(self, input_embedding):
-        if self.mode == 'train':
-            self.length = train_length
-        elif self.mode == 'valid':
-            self.length = valid_length
-        elif self.mode == 'test':
-            self.length = test_length
-        # 开始存储记忆向量
-        if self.memory_slot.size() == torch.Size([]):
-            self.memory_slot = input_embedding
-        else:
-            tor
 
 
 class ModelTrainer(object):
@@ -72,8 +64,8 @@ class ModelTrainer(object):
         self.model = model
         self.epochs = epochs
         self.writer = writer
-        self.best_model_path = "model/best_model.pth"
-        self.most_model_path = "model/most_model.pth"
+        self.best_model_path = model_save_path + "best_model.pth"
+        self.most_model_path = model_save_path + "most_model.pth"
         self.best_acc = 0.0
         self.train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0,
                                        pin_memory=True)  # num_workers=4, pin_memory=True 内存充足时使用，可以加速///1080只能设置num_works=0
@@ -121,7 +113,7 @@ class ModelTrainer(object):
                 train_accuracy += self.model_evaluate(true_labels, pred_labels)
                 # if (batch_index) % (self.steps // 50) == 0:  # 只打印五次结果
                 print('\r',
-                      "Epoch {:04d} | Step {:04d}/{:04d} | Train Cost Time {:.1f}s | End Time {:.1f}s | Step Time {:.1f}s | Loss {:.4f} | Accuracy {:.4f} | LR {:.4f}"
+                      "Epoch {:04d} | Step {:04d}/{:04d} | Train Cost Time {:.1f}s | End Time {:.1f}s | Step Time {:.1f}s | Loss {:.4f} | Accuracy {:.4f} | LR {}"
                       .format(epoch_index, batch_index, self.steps, time.time() - start_time,
                               (time.time() - start_time) / batch_index * (self.steps - batch_index),
                               (time.time() - start_time) / batch_index,
@@ -196,8 +188,8 @@ classify_model = SentenceClassifyModel(plm_path).to(device)
 # test_loader = DataLoader(test_dataset, batch_size=2, num_workers=0, pin_memory=True)
 # sample = next(iter(test_loader))
 # summary(classify_model, input_size=[(128,), (128,)])
-writer = SummaryWriter('runs/ClassifyModel2.0')  # tensorboard的可视化写入器以及对应的写入路径
-classify_model_train = ModelTrainer(model=classify_model, epochs=2, batch_size=4, writer=writer, is_visual=True)
+writer = SummaryWriter(summarywrite_path)  # tensorboard的可视化写入器以及对应的写入路径
+classify_model_train = ModelTrainer(model=classify_model, epochs=epochs, batch_size=batch_size, writer=writer)
 classify_model_train.model_train()
 classify_model_train.model_test()
 
--- memory_networks.py
+++ memory_networks.py
@@ -2,15 +2,17 @@ import torch
 import torch.nn as nn
 from torchsummary import summary
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-class MemoryNetwork(nn.Module):
+
+class MemoryNetworkLayer(nn.Module):
     def __init__(self):
-        super(MemoryNetwork, self).__init__()
+        super(MemoryNetworkLayer, self).__init__()
         self.softmax = nn.Softmax()
 
     def forward(self, memory_slot):
         if memory_slot.size()[0] == 1:
-            return memory_slot
+            return torch.cat((memory_slot, memory_slot), 1)
         u = memory_slot[-1:]
         m = memory_slot[:-1]
         um = u @ m.t()
@@ -19,7 +21,32 @@ class MemoryNetwork(nn.Module):
         return torch.cat((u, o), 1)
 
 
+class MemoryNetwork:
+    def __init__(self, hidden_size):
+        self.hidden_size = hidden_size
+        self.num_sentence = 0  # 计数句子序号
+        self.num_judgment = 0  # 计数文书序号
+        self.memory_slot = torch.empty(1, hidden_size).to(device)
+        self.memory_network_layer = MemoryNetworkLayer().to(device)
+
+    def get_memory_embedding(self, input_embedding, mode_length, mode):
+        length_list = mode_length[mode]  # 取哪个数据集  的  文本句子数量
+        # 开始存储记忆向量
+        if self.memory_slot.size() == input_embedding.size():  # 判断记忆槽是否为空
+            self.memory_slot = input_embedding
+        else:
+            self.memory_slot = torch.cat((self.memory_slot, input_embedding), 0)
+        memory_embedding = self.memory_network_layer(self.memory_slot)
+        # 开始计数
+        self.num_sentence += 1
+        if self.num_sentence >= length_list[self.num_judgment]:
+            self.num_sentence = 0
+            self.num_judgment += 1
+            self.memory_slot = torch.empty(1, self.hidden_size).to(device)
+        return memory_embedding
+
+
 if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = MemoryNetwork().to(device)
+    model = MemoryNetworkLayer().to(device)
     model(torch.rand(4, 768))
