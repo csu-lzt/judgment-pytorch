1bb5207e601416c5122625bb08587f515899b9d4
LZT <csulzt@foxmail.com>
2021-08-08 10:56:42 +0800

    读取句向量并白化

--- whitening/whitening.py
+++ whitening/whitening.py
@@ -3,16 +3,14 @@ import numpy as np
 
 def compute_kernel_bias(vecs):
     """计算kernel和bias
+    vecs.shape = [num_samples, embedding_size]，
     最后的变换：y = (x + bias).dot(kernel)
     """
-    vecs = np.concatenate(vecs, axis=0)
     mu = vecs.mean(axis=0, keepdims=True)
     cov = np.cov(vecs.T)
     u, s, vh = np.linalg.svd(cov)
     W = np.dot(u, np.diag(1 / np.sqrt(s)))
-    # return None, None
-    # return W, -mu
-    return W[:, :256], -mu
+    return W, -mu
 
 
 embeddings = np.load('whitening/embedding_avg.npy')
